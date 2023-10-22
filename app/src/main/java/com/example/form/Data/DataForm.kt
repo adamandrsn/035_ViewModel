51b0a33f551ca85f6a0206e9e62106610689f290
Adam Anderson Yuniarto <adam.anderson.ft21@mail.umy.ac.id>
2023-10-22 22:42:55 +0700

    DONE

---
+++ app/src/main/java/com/example/form/Data/DataForm.kt
@@ -0,0 +1,5 @@
+package com.example.form.Data
+
+data class DataForm(
+    val sex: String = ""
+)
---
+++ app/src/main/java/com/example/form/Data/DataSource.kt
@@ -0,0 +1,10 @@
+package com.example.form.Data
+
+import com.example.form.R
+
+object DataSource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
